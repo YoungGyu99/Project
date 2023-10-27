b4ea2ed35e6cbb8e8f42643d056f9ff8cb3dc4ff
YoungGyu99 <147577156+YoungGyu99@users.noreply.github.com>
2023-10-27 16:41:02 +0900

    Delete myProject directory

--- myProject/model/test.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-import time
-driver = webdriver.Chrome('chromedriver.exe')
-driver.implicitly_wait(3)
-url = 'http://www.naver.com'
-driver.get(url)
-time.sleep(1)
-print('a')
